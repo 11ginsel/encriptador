af600b535354a073c51e54241b44e6b468c7587d
11ginsel <125339994+11ginsel@users.noreply.github.com>
2023-02-20 10:09:22 -0600

    Add files via upload

---
+++ encriptar.js
@@ -0,0 +1,45 @@
+function encriptar() {
+  let texto = document.getElementById("texto").value;
+  let tituloMensaje = document.getElementById("titulo-mensaje");
+  let parrafo = document.getElementById("parrafo");
+
+  let textoCifrado = texto
+    .replace(/e/gi, "enter")
+    .replace(/i/gi, "imes")
+    .replace(/a/gi, "ai")
+    .replace(/o/gi, "ober")
+    .replace(/u/gi, "ufat");
+
+  if (texto.length != 0) {
+    document.getElementById("texto").value = textoCifrado;
+    tituloMensaje.textContent = "felicidad, el texto hacido encriptado ";
+    parrafo.textContent = "";
+  } else {
+    tituloMensaje.textContent = "no se a encontrado ningun texto";
+    parrafo.textContent = " por favor Ingresa el texto que deseas encriptar o desencriptar";
+    swal("error!", "vuelva intentalo de nuevo", "warning");
+  }
+}
+
+function desencriptar() {
+  let texto = document.getElementById("texto").value;
+  let tituloMensaje = document.getElementById("titulo-mensaje");
+  let parrafo = document.getElementById("parrafo");
+
+  let textoCifrado = texto
+    .replace(/enter/gi, "e")
+    .replace(/imes/gi, "i")
+    .replace(/ai/gi, "a")
+    .replace(/ober/gi, "o")
+    .replace(/ufat/gi, "u");
+  
+    if (texto.length != 0) {
+      document.getElementById("texto").value = textoCifrado;
+      tituloMensaje.textContent = "felicidad ,el texto hacido descriptado";
+      parrafo.textContent = "";
+    } else {
+      tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+      parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+      swal("error!", "vuelva intentarlo de nuevo ", "warning");
+    }
+}
